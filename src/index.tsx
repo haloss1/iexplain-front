510ee0946e7556de20f9ff0129f6b1f3f3d22589
Andrew Montoya <halossqwerty@gmail.com>
2021-07-11 14:31:04 -0600

    Changed relative imports to absolute

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import reportWebVitals from "./reportWebVitals";
-import Routes from "./Routes";
+import "index.css";
+import reportWebVitals from "reportWebVitals";
+import Routes from "Routes";
 
 ReactDOM.render(
   <React.StrictMode>
