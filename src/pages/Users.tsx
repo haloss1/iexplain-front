f227324b8303526b0fe85ab2565b5548ad0eeda5
Andrew Montoya <halossqwerty@gmail.com>
2021-07-11 22:26:42 -0600

    Added User list

--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -1,5 +1,32 @@
+import { Card, Avatar, CardHeader, Button } from "@material-ui/core";
+import styled from "styled-components";
+
+const GridContainer = styled.div`
+  display: grid;
+  padding: 1rem;
+  gap: 1rem;
+  grid-template-columns: repeat(auto-fit, minmax(240px, 1fr));
+`;
+
 const Users = () => {
-  return <div></div>;
+  return (
+    <div>
+      <GridContainer>
+        <Card>
+          <CardHeader
+            avatar={<Avatar>M</Avatar>}
+            title="Michael J."
+            subheader="example@example.com"
+          />
+        </Card>
+      </GridContainer>
+      <div style={{ textAlign: "center", paddingBottom: "1rem" }}>
+        <Button variant="contained" color="primary">
+          Load More
+        </Button>{" "}
+      </div>
+    </div>
+  );
 };
 
 export default Users;
